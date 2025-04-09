bdc1c960660d4d608d59575c1518cb212c13d9de
Shaikrizwan0116 <shaikrizwan0116@gmail.com>
2025-04-09 12:38:38 +0530

    check leap year

---
+++ Leap_year.js
@@ -0,0 +1,14 @@
+function isLeapYear(year){
+    if(year%4===0 && year % 100 !== 0){
+        return `${year} is a leap year`
+    }
+    else if(year % 400 == 0){
+        return `${year} is a leap year`
+    }
+    else{
+        return `${year} is not a leap year`
+    }
+}
+
+console.log(isLeapYear(2024));
+console.log(isLeapYear(2025));
